22c3c82a78561ff574c08cd50a3c80b3b41e2098
MaazMohamedy <maaz.mohamedy@gmail.com>
2018-11-03 17:01:28 -0700

    added manhattan and misplaced tile heauristics

--- eightPuzzle.py
+++ eightPuzzle.py
@@ -1,7 +1,8 @@
 import heapq
-from copy import copy, deepcopy
+# from copy import copy, deepcopy
 
 def main():
+	
 
 	puzzleType = input("Welcome to Maaz Mohamedy's 8-puzzle solver.\nType '1' to use default puzzle," +
 		" or '2' to create your own. \n")
@@ -19,12 +20,54 @@ def main():
 		"\n\t2. A* with the Misplaced Tile heuristic." +
 		"\n\t3. A* with the Manhattan distance heuristic.\n")
 
-	if (algo == "1"):
-		uniformCostSearch(puzzle,algo)
+	generalSearch(puzzle,algo)
 
 
+def misplacedTileHeuristic(puzzle):
 
-def expandUCS(node):
+	numMisplaced = 0;
+	val = 1
+	for i in range(0,len(puzzle)):
+		for j in range(0,len(puzzle[i])):
+			if val == 9: val = 0
+			if puzzle[i][j] != val and puzzle[i][j] != 0:
+				numMisplaced = numMisplaced + 1
+			val = val + 1
+
+	return numMisplaced
+
+def manhattanDistanceHeuristic(puzzle):
+	val = 1
+	manhattanSum = 0
+	for i in range(0,len(puzzle)):
+		for j in range(0,len(puzzle[i])):
+			if val == 9: val = 0
+			if puzzle[i][j] != val and puzzle[i][j] != 0:
+				x,y = findIndicesOfGoalState(puzzle[i][j])
+				horizontal = abs(j - x)
+				vertical = abs(i - y)
+				moves  = horizontal + vertical
+				manhattanSum = manhattanSum + moves
+			val = val + 1
+
+	return manhattanSum
+
+def findIndicesOfGoalState(val):
+	goal = [[1,2,3], [4,5,6], [7,8,0]]
+	for i in range(0,len(goal)):
+		for j in range(0,len(goal[i])):
+			if goal[i][j] == val:  return(j,i)
+
+def calculateHeuristic(algo, puzzle):
+	if algo == "1": return 0
+
+	if algo == "2":
+		return misplacedTileHeuristic(puzzle)
+
+	if algo == "3":
+		return manhattanDistanceHeuristic(puzzle)
+
+def expand(node, algo):
 	children = []
 	currPuzzle = node[1]
 
@@ -37,40 +80,67 @@ def expandUCS(node):
 				x,y = i,j
 				break
 
-	#shift blank updatedCost
-	if x-1 >= 0:
-		shiftUp = list(map(list, node[1],))	
-		shiftUp[x][y], shiftUp[x-1][y] = shiftUp[x-1][y] , shiftUp[x][y]
-		updatedCost = node[0] + 1
-		children.append((updatedCost,shiftUp))
-
-	# print()
-	#shift blank down
+	
 	if x+1 < (len(node[1])):
+		# shift blank space down
 		shiftDown = list(map(list, node[1],))	
 		shiftDown[x][y], shiftDown[x+1][y] = shiftDown[x+1][y] , shiftDown[x][y]
-		updatedCost = node[0] + 1
-		children.append((updatedCost,shiftDown))
+		# calculate hn and gn
+		hOfN = calculateHeuristic(algo, shiftDown)
+		gOfN = node[2] + 1
+		updatedCost = gOfN + hOfN
+		print(shiftDown)
+		print("shift down")
+		print("H(n): " + str(hOfN))
+		print("G(n): " + str(gOfN))
+		children.append((updatedCost,shiftDown,gOfN))
 
-	# print()	
-	#shift blank left
 	if y-1 >= 0:
+		# shift blank space left
 		shiftLeft = list(map(list, node[1],))	
 		shiftLeft[x][y], shiftLeft[x][y-1] = shiftLeft[x][y-1] , shiftLeft[x][y]
-		updatedCost = node[0] + 1
-		children.append((updatedCost,shiftLeft))
+		# calculate hn and gn
+		hOfN = calculateHeuristic(algo, shiftLeft)
+		gOfN = node[2] + 1
+		updatedCost = gOfN + hOfN
+		print("shift left")
+		print(shiftLeft)
+		print("H(n): " + str(hOfN))
+		print("G(n): " + str(gOfN))
+		children.append((updatedCost,shiftLeft,gOfN))
 
-	# print()
-	#right
+	if x-1 >= 0:
+		# shift blank space up
+		shiftUp = list(map(list, node[1],))	
+		shiftUp[x][y], shiftUp[x-1][y] = shiftUp[x-1][y] , shiftUp[x][y]
+		# calculate hn and gn
+		hOfN = calculateHeuristic(algo, shiftUp)
+		gOfN = node[2] + 1
+		updatedCost = gOfN + hOfN
+		print("shift up")
+		print(shiftUp)
+		print("H(n): " + str(hOfN))
+		print("G(n): " + str(gOfN))
+		children.append((updatedCost,shiftUp,gOfN))
+
+
 	if y+1 < (len(node[1])):
+		# shift blank space right
 		shiftRight = list(map(list, node[1],))	
 		shiftRight[x][y], shiftRight[x][y+1] = shiftRight[x][y+1] , shiftRight[x][y]
-		updatedCost = node[0] + 1
-		children.append((updatedCost,shiftRight))
+		# calculate hn and gn
+		hOfN = calculateHeuristic(algo, shiftRight)
+		gOfN = node[2] + 1
+		updatedCost = gOfN + hOfN
+		print("shift right")
+		print(shiftRight)
+		print("H(n): " + str(hOfN))
+		print("G(n): " + str(gOfN))
+		children.append((updatedCost,shiftRight, gOfN))
 
 	return children
 
-def uniformCostSearch(puzzle, algo):
+def generalSearch(puzzle, algo):
 
 	#4^(d+1) - 5
 
@@ -78,41 +148,48 @@ def uniformCostSearch(puzzle, algo):
 	failure = False;
 	maxQueueLen = 0;
 
-	# Uniform cost search
-	if (algo == "1"):
-		nodes = []
+	nodes = []
 
+	heapq.heappush(nodes, (0, puzzle, 0) )
 
+	while(True):
+		if len(nodes) == 0: #if nodes is empty, return failure
+			failure = True
+			break 
 
-		heapq.heappush(nodes, (0, puzzle) )
-		while(True):
+		if maxQueueLen < len(nodes): maxQueueLen = len(nodes)
+		currNode = heapq.heappop(nodes)
 
-			#if nodes is empty, return failure
-			if len(nodes) == 0:
-				failure = True
-				break 
+		if currNode[1] == goalState:
+			print("DEPTH: ")
+			print(currNode[2])
+			# print("Nodes: ")
+			# print(nodes)
+			print("Max len nodes:")
+			print(maxQueueLen)
+			print("Max len nodes:")
+			return currNode;
 
-			if maxQueueLen < len(nodes): maxQueueLen = len(nodes)
-			currNode = heapq.heappop(nodes)
+		children = expand(currNode, algo)
 
-			if currNode[1] == goalState:
-				print("DEPTH: ")
-				print(currNode[0])
-				print("Nodes: ")
-				print(nodes)
-				print("Max len nodes:")
-				print(maxQueueLen)
-				return currNode;
+		for child in children:
+			heapq.heappush(nodes, child)
 
-			# expand currNode's children'
+		print(nodes)
 
-			children = expandUCS(currNode)
+		# x  = input("take a breath: ")
 
-			for child in children:
-				heapq.heappush(nodes, child)
+	# printResults()
+
+	if failure:
+		print("FAILURE")
 
 	return
 		
+
+# def printResults():
+
+
 def pickCustomArrangement():
 	puzzle = []
 	print("Enter your puzzle, use a zero to represent the blank")
